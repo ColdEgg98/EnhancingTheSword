8ae4970b10ae10896cf9a05db53e0039d24cc0b8
ColdEgg98 <145242248+ColdEgg98@users.noreply.github.com>
2025-12-01 01:15:04 +0900

    [Feat] xlsx 리딩 실행 기능

--- Assets/02. Scripts/RunTime/Systems/XlsxDataReader.cs
+++ Assets/02. Scripts/RunTime/Systems/XlsxDataReader.cs
@@ -13,17 +13,23 @@ public static class XlsxDataReader<T> where T : new()
         var worksheet = workbook.Worksheet(sheetIndex);
 
         // 첫 번째 행을 헤더로 사용
-        var headers = worksheet.Row(1).Cells().Select(c => c.GetString()).ToList();
+        var headerOrigins = worksheet.Row(1).Cells().Select(c => c.GetString()).ToList();
+
+        // 헤더에 중복이 있을경우 걸러줌 (대소문자 무시)
+        List<string> headers = headerOrigins
+            .Distinct(StringComparer.OrdinalIgnoreCase)
+            .ToList<string>();
 
         foreach (var row in worksheet.RowsUsed().Skip(1)) // 헤더 제외
         {
             var obj = new T();
-            var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            // Reflection 활용. 들어온 클래스의 퍼블릭 인스턴스 변수를 PropertyInfo[] 구조로 그룹화
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             for (int i = 0; i < headers.Count; i++)
             {
                 string header = headers[i];
-                var prop = props.FirstOrDefault(p => p.Name.Equals(header, StringComparison.OrdinalIgnoreCase));
+                PropertyInfo prop = props.FirstOrDefault(p => p.Name.Equals(header, StringComparison.OrdinalIgnoreCase));
                 if (prop != null)
                 {
                     string cellValue = row.Cell(i + 1).GetString();
