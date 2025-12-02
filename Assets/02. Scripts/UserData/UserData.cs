142bc88ba8f28eebd35493b755e2ebfc76f22b58
ColdEgg98 <leytriss@gmail.com>
2025-12-03 05:10:14 +0900

    [Feat] 인벤토리에 보유 무기를 표시하는 기능
    이 때 rgb값이 0으로 설정되는 이슈가 있었음 왠지 몰루

--- Assets/02. Scripts/UserData/UserData.cs
+++ Assets/02. Scripts/UserData/UserData.cs
@@ -1,8 +1,10 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 [Serializable]
 public class UserData
@@ -49,10 +51,28 @@ public class WrapperForPreviewData
 public class Weapon
 {
     public int index { get; set; }
-    public string name {  get; set; }
+    public string name { get; set; }
     public double price { get; set; }
     public float probability { get; set; }
     public double enhancingPrice { get; set; }
     public string addressID { get; set; }
     public List<int> needItems { get; set; }
+    private Sprite sprite;
+
+    public async Awaitable<Sprite> GetWeaponSpriteAsync()
+    {
+        AsyncOperationHandle<Sprite> handle = Addressables.LoadAssetAsync<Sprite>(addressID);
+        sprite = await handle.Task;
+        return sprite;
+    }
+    public async Task ApplySpriteToImage(Image targetImage)
+    {
+        // 내부에서 await로 풀어서 처리
+        Sprite sprite = await GetWeaponSpriteAsync();
+
+        if (targetImage != null)
+        {
+            targetImage.sprite = sprite;
+        }
+    }
 }
