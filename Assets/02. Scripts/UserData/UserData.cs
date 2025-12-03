0213c6b96960fb7faaf3f6963ec609bfbb3b7df6
ColdEgg98 <145242248+ColdEgg98@users.noreply.github.com>
2025-12-03 23:57:36 +0900

    [Feat] 업적 UI 호출 기능

--- Assets/02. Scripts/UserData/UserData.cs
+++ Assets/02. Scripts/UserData/UserData.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 [Serializable]
 public class UserData
@@ -55,14 +58,30 @@ public class Weapon
 }
 
 [Serializable]
-public class Achivement
+public class Achivement : IDisposable
 {
     public string achivementID { get; set; }
     public string description { get; set; }
     public float probabilityPlus { get; set; }
-    public Sprite achivementSprite { get; set; }
-    public async Task AchivementSpriteApply()
+    private AsyncOperationHandle<Sprite> _handle;
+    public async Task AchivementSpriteApply(Image targetImage)
     {
-        
+        if (_handle.IsValid())
+            Addressables.Release(_handle);
+
+        _handle = Addressables.LoadAssetAsync<Sprite>(achivementID);
+        await _handle.Task;
+        targetImage.sprite = _handle.Result;
+    }
+
+    // 사용하는 UI에서 Dispose를 잘해줘야함.
+    // 업적 이미지가 내려갈 때 Dispose를 호출하게되거나
+    // 업적 Monobehavior 상속된 핸들러 클래스를 만들어서 업적 UI에 붙이면될듯
+    // 만약 이 솔루션으로 되면 dispose대신 ondestory 쓰면 되고
+    public void Dispose()
+    {
+        if (_handle.IsValid())
+            Addressables.Release(_handle);
+        _handle = default;
     }
 }
