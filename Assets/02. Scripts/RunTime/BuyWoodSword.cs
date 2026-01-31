d4cde56ed33d861be9429e2441dae5c09ca358cd
ColdEgg98 <145242248+ColdEgg98@users.noreply.github.com>
2026-02-01 04:12:03 +0900

    [Fix] 무기 제한

--- Assets/02. Scripts/RunTime/BuyWoodSword.cs
+++ Assets/02. Scripts/RunTime/BuyWoodSword.cs
@@ -13,7 +13,14 @@ public class BuyWoodSword : MonoBehaviour
 
     private void BuySword()
     {
-        GameManager.Instance.userDataManager.GetGold(-10000);
-        GameManager.Instance.userDataManager.GetWeapon(1);
+        if (GameManager.Instance.currentData.myWeapons.Count < 20)
+        {
+            GameManager.Instance.userDataManager.GetGold(-10000);
+            GameManager.Instance.userDataManager.GetWeapon(1);
+        }
+        else
+        {
+            GameManager.Instance.uiManager.UIFactory.ShowNotice("인벤토리가 가득 찼습니다.");
+        }
     }
 }
